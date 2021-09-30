734852b4723f6c37283569a29a1fd9e86c72ebad
het <het@hulkapps.com>
2021-09-30 17:50:41 +0530

    Order Create and order cancel job merged using trait.

---
+++ app/Models/AvalaraTransactionLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AvalaraTransactionLog extends Model
+{
+    use HasFactory;
+
+    protected $table = 'avalara_transaction_log';
+}
